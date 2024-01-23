d5ea9544bc3eb9296ca2135644f8c92ce4af1886
chukwucyprian <chukwucyprian262@gmail.com>
2024-01-23 22:40:48 +0100

    correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,43 @@
 #!/usr/bin/python3
+"""
+Log parsing script.
+Parses log lines from stdin and computes metrics.
+"""
 import sys
 
-
-def print_stats(total_size, status_code_counts):
-    print("File size: {}".format(total_size))
-    for code in sorted(status_code_counts.keys()):
-        print("{}: {}".format(code, status_code_counts[code]))
-
-
-def parse_line(line, total_size, status_code_counts):
-    try:
-        parts = line.split()
-        if len(parts) != 9 or parts[2] != "GET" or parts[3] \
-                != "/projects/260" or parts[5] != "HTTP/1.1":
-            return total_size, status_code_counts  # Skip invalid lines
-
-        status_code = int(parts[7])
-        file_size = int(parts[8])
-
-        total_size += file_size
-
-        if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
-            status_code_counts[status_code] = \
-                status_code_counts.get(status_code, 0) + 1
-
-    except ValueError:
-        pass  # Ignore lines with invalid integer values
-
-    return total_size, status_code_counts
+possible_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+lines_read = 0
+status_codes_map = {}
+total_file_size = 0
 
 
-def main():
-    total_size = 0
-    status_code_counts = {}
-
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            total_size, status_code_counts = parse_line(line.strip(),
-                                                total_size, status_code_counts)
-            if i % 10 == 0:
-                print_stats(total_size, status_code_counts)
-
-    except KeyboardInterrupt:
-        pass  # Handle CTRL + C
-
-    finally:
-        print_stats(total_size, status_code_counts)
+def print_stats():
+    """prints out the statistics"""
+    print("File size: {}".format(total_file_size))
+    for status, count in sorted(status_codes_map.items()):
+        print("{}: {}".format(status, count))
 
 
-if __name__ == "__main__":
-    main()
+try:
+    for line in sys.stdin:
+        line_tokens = line.split()
+        try:
+            file_size = int(line_tokens[-1])
+            total_file_size += file_size
+            status_code = int(line_tokens[-2])
+            if status_code in possible_status_codes:
+                if status_code in status_codes_map:
+                    status_codes_map[status_code] += 1
+                else:
+                    status_codes_map[status_code] = 1
+        except ValueError:
+            print("Warning: Ignored line with unexpected format:", line)
+        lines_read += 1
+        if lines_read % 10 == 0:
+            print_stats()
+
+    if (lines_read == 0) or (lines_read % 10 != 0):
+        print_stats()
+
+except (KeyboardInterrupt):
+    print_stats()
