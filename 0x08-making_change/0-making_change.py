53f729ac584e804b2e5788344d911266d5d891f4
chukwucyprian <chukwucyprian262@gmail.com>
2024-02-28 01:16:00 +0100

    more_correction

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,19 +7,17 @@ def makeChange(coins, total):
     Given a pile of coins of different values,
     determine the fewest number of coins needed to meet a given amount tota
     """
-    if total <= 0:
+    if total < 1:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    coins.sort()
+    coins.reverse()
+    fewest = 0
     for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
-
-
-if __name__ == "__main__":
-    print(makeChange([1, 2, 25], 37))
-    print(makeChange([1256, 54, 48, 16, 102], 1453))
+        if total <= 0:
+            break
+        buff = total // coin
+        fewest += buff
+        total -= (buff * coin)
+    if total != 0:
+        return -1
+    return fewest
